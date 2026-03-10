07add8073f59c1cd9e164bec3b5c42cf8f43962b
Polina <nikulschina-p@yandex.ru>
2026-03-10 12:47:58 +0400

    checkStyle

--- main-service/src/main/java/ru/practicum/user/service/UserAdminService.java
+++ main-service/src/main/java/ru/practicum/user/service/UserAdminService.java
@@ -1,6 +1,5 @@
 package ru.practicum.user.service;
 
-import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import ru.practicum.user.dto.UserDto;
 import ru.practicum.user.dto.UserRequestDto;
