b5b1f8c75b711cde5fc3819b5357ca9b1d47b637
Polina <nikulschina-p@yandex.ru>
2026-03-12 09:54:54 +0400

    checkStyle

--- main-service/src/main/java/ru/practicum/user/service/UserAdminServiceImpl.java
+++ main-service/src/main/java/ru/practicum/user/service/UserAdminServiceImpl.java
@@ -3,7 +3,6 @@ package ru.practicum.user.service;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.domain.Pageable;
-import org.springframework.expression.spel.ast.NullLiteral;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import ru.practicum.exception.ConflictException;
